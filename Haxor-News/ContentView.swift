718b4490d1b7c38ca5248250bfbd4c68575f6238
Fernando Gonzalez <ing.fernandogonzalez@outlook.com>
2021-09-05 13:36:30 -0500

    Initial Commit

---
+++ Haxor-News/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Haxor-News
+//
+//  Created by Fernando González on 05/09/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}

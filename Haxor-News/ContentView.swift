3019eea6fcba52073b5a25e05f41b4e80234797e
Fernando Gonzalez <ing.fernandogonzalez@outlook.com>
2021-09-06 13:54:48 -0500

    Struct Post with Identifieable protocol was implemented. Part 1.

--- Haxor-News/ContentView.swift
+++ Haxor-News/ContentView.swift
@@ -3,19 +3,41 @@
 //  Haxor-News
 //
 //  Created by Fernando González on 05/09/21.
-//
+// git repository: https://github.com/FernandoGonz/Haxor-News-SwiftUI.git
 
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            List(posts) { post in
+                // for any post in posts List do:
+                Text(post.title)
+            }
+            .navigationBarTitle("HAXOR NEWS")
+        }
     }
 }
+
+/** Creating a Post struct with protocol Identifiable */
+/* This protocol allows the list can recognizes the order of our objects. */
+struct Post: Identifiable {
+    // You need an identifier to Identifiable protocol
+    let id: String
+    let title: String
+}
+
+/* Creting a List post of Post struct */
+let posts: [Post] = [
+    Post(id: "1", title: "Hola"),
+    Post(id: "2", title: "Mundo"),
+    Post(id: "3", title: "Bye Mundo")
+]
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .previewDevice(PreviewDevice(rawValue: "iPhone XR"))
     }
 }
+
---
+++ Haxor-News/Post.swift
@@ -0,0 +1,24 @@
+//
+//  Post.swift
+//  Haxor-News
+//
+//  Created by Fernando González on 06/09/21.
+//
+
+import SwiftUI
+
+struct Post: View {
+    
+    let id: String
+    let title: String
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Post_Previews: PreviewProvider {
+    static var previews: some View {
+        Post().previewLayout(.sizeThatFits)
+    }
+}
